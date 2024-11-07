5406fc2d7770809f23b0dda10e74c33253ed6bc8
eresdavid05 <eresodavid05@gmail.com>
2024-11-07 15:12:51 -0500

    Cleaned up dataset

--- data/metadata/baseline.py
+++ data/metadata/baseline.py
@@ -1,16 +1,20 @@
-#Determining dates
+#Baseline Determining Functions 
 #Date Range: dates from which baseline days are selected 
+
 #Weekends?: Yes or no (Can measurements be taken on weekdends?)
+weekends: True/False 
 #Holidays? Yes or no (can measurements be taken on holidays?)
+holidays: True/False 
+#Previous events? 
+prev_events: True/False 
 #Time range: time of day during which load can be measured 
+base_hours: 0-24 
 #Load measurement frequency - minutes vs. hourly vs. monthly: how often load is measured 
+range_resolution: "month", "days", "hours", "minutes" 
+range_value: #
 #Load measurement function - avg vs. max: function applied to load measurements in a given time range and date range
-
-
-
-#Applying function to dates 
-#Avg vs. Median: apply either avg or med function to all measurements that fit the parameters outlined above for baseline load 
+function: mean(), max(), min(), median()
 
 #Alternative: Firm Level Demand value is baseline; Firm level demand is a baseline value that the company has to meet in order to receive payment
 #Add another variable called firm level demand that will be used in the payment function instead of baseline. (payment function
-#will either use baseline or firm level demand)
+#will either use baseline or firm level demand)F
